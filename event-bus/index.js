439ed8fe6ebf0ed20ea307a35d779c4dfd5b9570
Brian <lcy@BriandeMacBook-Pro.local>
2023-08-01 10:53:40 +0800

    Update backend work flow through moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,8 +17,12 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((err) => {
     console.log(err.message);
   });
+  //   moderation services
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
-});
+}); 
 
 app.listen(4005, () => {
   console.log("Listening on 4005");
